be2ab4308e62cc376a02b42c7c95f3305715a930
ATroshkin <troshkinap@vtb.ru>
2022-11-20 14:28:44 +0300

    math operation

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма чисел = " + (firstNumber + secondNumber));
+        System.out.println("Разность чисел = " + (firstNumber - secondNumber));
+        System.out.println("Произведение чисел = " + (firstNumber * secondNumber));
+        System.out.println("Частное чисел = " + ((double) firstNumber / secondNumber));
     }
 }
