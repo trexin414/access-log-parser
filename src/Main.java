580557796e60e6bc38cf3a142fb8e5b78a321a9c
ATroshkin <troshkinap@vtb.ru>
2022-12-25 20:34:38 +0300

    hw6.2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+    private static final String YANDEX_BOT = "YandexBot";
+    private static final String GOOGLE_BOT = "GoogleBot";
+    private static final String GOOGLE_BOT_SMALL = "Googlebot";
     public static void main(String[] args) {
-        int count = 1;
-        String path;
+        int count = 1, googlebotCount = 0, yandexCount = 0, googleBotCount = 0;
+        String path = "access.log", ip = "", date = "", method = "", httpCode = "", size = "", httpPage = "", uAgent = "", comp = "", fragment = "";
         List<String> lines = new ArrayList<>();
-        int max = 0, min = 1024;
+        HashMap<String, HashMap<String, String>> mapLine = new HashMap<>();
+        Pattern ipPattern = Pattern.compile("^(\\d{1,3}[\\.]){3}\\d{1,3}");
+        Pattern datePattern = Pattern.compile("\\[((\\w+\\/){2}(\\d+\\:){3}\\d+\\ (\\+|-)\\d+)]");
+        Pattern methodPattern = Pattern.compile("GET\\s+\\/.*\\HTTP\\/(\\w.\\w|\\w)");
+        Pattern codeAndSizePattern = Pattern.compile("\\ (\\d*) (\\d*) ");
+        Pattern httpPagePattern = Pattern.compile("\"https:.*\"");
+        Pattern compatiblePattern = Pattern.compile("(compatible;.*)\\)");
+        Pattern botPattern = Pattern.compile("(\\w+)\\/");
+
+        Matcher matcher;
 
         while (true){
-            System.out.println("Введите путь до файла:");
-            path = new Scanner(System.in).nextLine();
+//            System.out.println("Введите путь до файла:");
+//            path = new Scanner(System.in).nextLine();
 
             File file = new File(path);
             boolean fileExists = file.exists();
@@ -35,18 +47,55 @@ public class Main {
             BufferedReader reader = new BufferedReader(fileReader);
             String line;
             while ((line = reader.readLine()) != null) {
-                int length = line.length();
-                if (length > 1024) throw new LengthExceptions("Длина строки " + (lines.size() + 1) + " больше 1024 символов");
-                if (length < min) min = length;
-                if (length > max) max = length;
+                if (line.length() > 1024) throw new LengthExceptions("Длина строки " + (lines.size() + 1) + " больше 1024 символов");
                 lines.add(line);
             }
-            System.out.println("Кол-во строк в файле = " + lines.size());
-            System.out.println("Максимальное число символов в строке = " + max);
-            System.out.println("Минимальное число символов в строке = " + min);
         } catch (IOException | LengthExceptions exception){
             exception.printStackTrace();
         }
 
+        for (String line: lines){
+            matcher = ipPattern.matcher(line);
+            if (matcher.find()) ip = matcher.group();
+
+            matcher = datePattern.matcher(line);
+            if (matcher.find()) date = matcher.group(1);
+
+            matcher = methodPattern.matcher(line);
+            if (matcher.find()) method = matcher.group();
+
+            matcher = codeAndSizePattern.matcher(line);
+            if (matcher.find()) {
+                httpCode = matcher.group(1);
+                size = matcher.group(2);
+            }
+
+            matcher = httpPagePattern.matcher(line);
+            if(matcher.find()) httpPage = matcher.group();
+
+            uAgent = line.substring(line.lastIndexOf("\"", line.lastIndexOf("\"") - 1) + 1, line.lastIndexOf("") - 1);
+
+            matcher = compatiblePattern.matcher(uAgent);
+            if (matcher.find()) comp = matcher.group(1);
+
+            List<String> parts = Arrays.asList(comp.split(";"));
+
+            parts.replaceAll(s -> s.replace(" ", ""));
+
+            for(String s: parts){
+                matcher = botPattern.matcher(s);
+                if (matcher.find()) {
+                    fragment = matcher.group(1);
+                    break;
+                }
+            }
+
+            if(fragment.equals(YANDEX_BOT)) yandexCount++;
+            if(fragment.equals(GOOGLE_BOT)) googleBotCount++;
+            if(fragment.equals(GOOGLE_BOT_SMALL)) googlebotCount++;
+        }
+        System.out.println("Доля запросов от YandexBot = " + (double) yandexCount / lines.size());
+        System.out.println("Доля запросов от GoogleBot = " + (double) googleBotCount / lines.size());
+        System.out.println("Доля запросов от Googlebot = " + (double) googlebotCount / lines.size());
     }
 }
